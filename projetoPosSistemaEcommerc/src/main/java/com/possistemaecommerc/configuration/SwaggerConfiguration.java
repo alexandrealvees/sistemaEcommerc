5a3bb4a8ddafdf7fd58b45a5c619654c60571b6d
Walace Lima <weclima@gmail.com>
2024-02-09 14:53:18 -0300

    Criação de configuração para Swagger.

--- projetoPosSistemaEcommerc/src/main/java/com/possistemaecommerc/configuration/SwaggerConfiguration.java
+++ projetoPosSistemaEcommerc/src/main/java/com/possistemaecommerc/configuration/SwaggerConfiguration.java
@@ -1,26 +1,29 @@
-//package com.possistemaecommerc.configuration;
-//
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfiguration {
-//    //método para configurar a geração da documentação..
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//
-//    }
-//}
+package com.possistemaecommerc.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfiguration {
+    //método para configurar a geração da documentação..
+        @Bean
+        public OpenAPI defineOpenApi() {
+            Server server = new Server();
+            server.setUrl("http://localhost:8080");
+            server.setDescription("Projeto fase 5 para pós graduação em Arquitetura de Sistemas Java");
+
+
+            Info information = new Info()
+                    .title("Projeto PosTech Fiap Fase-5")
+                    .version("1.0.0")
+                    .description("Esta Api expoe os endpoints para gerenciamento.");
+            return new OpenAPI().info(information).servers(List.of(server));
+        }
+}
